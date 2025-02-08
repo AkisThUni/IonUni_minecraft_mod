8a95a7f248576a34fcae96f0800720d78655f757
Kapetanbanananoflouda54 <tablet.sifi@gmail.com>
2025-02-08 04:45:47 +0200

    Added Clover Crisp Clover to creative tab

--- forge-1.21-51.0.33-mdk/src/main/java/com/kapetanbananoflouda/minecraftmod/items/ModCreativeModeTabs.java
+++ forge-1.21-51.0.33-mdk/src/main/java/com/kapetanbananoflouda/minecraftmod/items/ModCreativeModeTabs.java
@@ -32,6 +32,8 @@ public class ModCreativeModeTabs {
                         output.accept(ModItems.XANAX_PACKET.get());
                         output.accept(ModItems.DICE_BLADE.get());
                         output.accept(ModItems.DICE_PICK.get());
+                        output.accept(ModItems.CLOVER.get());
+                        output.accept(ModItems.CRISP_CLOVER.get());
 
 
                     }).build());
